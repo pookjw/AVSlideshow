b6a655722243f47b4c89483123f7f4cf242cdd8f
Jinwoo Kim <kidjinwoo@me.com>
2023-02-27 16:20:47 +0900

    no message

--- AVSlideshow_iOS/AVSlideshow_iOS/Services/SlideshowService.swift
+++ AVSlideshow_iOS/AVSlideshow_iOS/Services/SlideshowService.swift
@@ -10,6 +10,25 @@ import AVFoundation
 
 actor SlideshowService {
     func exportVideo(from images: [UIImage]) async throws -> Data {
-        .init()
+        let duration: CMTime = .init(
+            seconds: 3.0,
+            preferredTimescale: 600
+        )
+        
+        let timeRange: CMTimeRange = .init(
+            start: .zero,
+            duration: duration
+        )
+        
+        let videoSize: CGSize = .init(width: 1_280, height: 720)
+        
+        let composition: AVMutableComposition = .init()
+        let rootLayer: CALayer = .init()
+        rootLayer.frame = .init(origin: .zero, size: videoSize)
+        
+        let videoComposition: AVMutableVideoComposition = .init()
+        videoComposition.renderSize = videoSize
+        
+        return .init()
     }
 }
